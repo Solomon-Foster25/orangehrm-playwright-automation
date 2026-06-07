d46b0e932efc1d66eef107636752629f43f17c3d
Solomon Foster <solomon.foster25@gmail.com>
2026-06-06 18:44:36 -0600

    Add LoginPage with login locators and methods, as well as BasePage class

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,4 +1,35 @@
-export class LoginPage () {
+import { BasePage } from "./BasePage";
 
+export class LoginPage extends BasePage{
+    // Initialize locators in constructor
 
-}
+    constructor(page) {
+        this.page = page; // Initializes page
+        this.usernameInput = page.getByPlaceholder('Username'); // Locates username
+        this.passwordInput = page.getByPlaceholder('Password'); // Locates password
+        this.loginButton = page.getByRole('button', {name: 'Login'}); // Locates login button
+        this.forgotButton = page.getByText('Forgot your password? '); // Locates forgot your password
+        
+    }
+
+    // Navigate to login page
+
+    async goto() {
+        await this.page.goto('/web/index.php/auth/login'); 
+    }
+
+    // Fill in credentials and submit the form
+
+    async login(username, password) {
+        await this.usernameInput.fill(username); // Fills username
+        await this.passwordInput.fill(password); // Fills password
+        await this.loginButton.click(); // CLicks login button
+    };
+
+    // Goes to Forgot password
+
+    async forgotPassword() {
+        await this.forgotButton.click(); // Forgot password button
+    };
+
+};
