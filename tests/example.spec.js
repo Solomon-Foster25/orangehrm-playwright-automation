2436e4f51c157bdb168eac3d8f2e923d36183e76
Solomon Foster <solomon.foster25@gmail.com>
2026-06-07 19:36:55 -0600

    Changed to login smoke test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,9 @@
-// @ts-check
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('valid login lands on dashboard', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('Admin', 'admin123');
+  await expect(page).toHaveURL(/dashboard/);
 });
