317820b4e91a7414fb94c93722010de6bf08dc76
Solomon Foster <solomon.foster25@gmail.com>
2026-06-07 18:01:04 -0600

    Add locators and class

---
+++ pages/DashboardPage.js
@@ -0,0 +1,21 @@
+import { expect } from "@playwright/test";
+
+export class DashboardPage {
+    // Initialize locators in constructor
+
+    constructor(page) {
+        this.page = page; // Initializes page
+        this.dashboardTitle = page.getByRole('heading', {name: 'Dashboard'}); // Locates title 'Dashboard'
+        this.timeAtWork = page.getByText('Time at Work'); // Locates tite 'Time at Work'
+        // Quick Launch selectors
+
+        this.assignLeave = page.getByRole('button', {name: 'Assign Leave'});
+        this.leaveList = page.getByRole('button', {name: 'Leave List'});
+        this.timeSheets = page.getByRole('button', {name: 'Timesheets'});
+        this.applyLeave = page.getByRole('button', {name: 'Apply Leave'});
+        this.myLeave = page.getByRole('button', {name: 'My Leave'});
+        this.myTimesheet = page.getByRole('button', {name: 'My Timesheet'});
+    };
+
+
+}
