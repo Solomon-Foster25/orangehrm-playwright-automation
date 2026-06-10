c77733cf1bbce2ac61ff16068115c48418efd13c
Solomon Foster <solomon.foster25@gmail.com>
2026-06-10 11:09:41 -0600

    Added locators for widgets

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -6,15 +6,21 @@ export class DashboardPage extends BasePage {
     constructor(page) {
         super(page); // Initializes page
         this.dashboardTitle = page.getByRole('heading', {name: 'Dashboard'}); // Locates title 'Dashboard'
-        this.timeAtWork = page.getByText('Time at Work'); // Locates tite 'Time at Work'
-        // Quick Launch selectors
+        this.timeAtWork = page.getByText('Time at Work'); // Locates widget 'Time at Work'
+                
+        this.myActions = page.getByText('My Actions'); // Locates widget "My Actions"
 
-        this.assignLeave = page.getByRole('button', {name: 'Assign Leave'});
-        this.leaveList = page.getByRole('button', {name: 'Leave List'});
-        this.timeSheets = page.getByRole('button', {name: 'Timesheets'});
-        this.applyLeave = page.getByRole('button', {name: 'Apply Leave'});
-        this.myLeave = page.getByRole('button', {name: 'My Leave'});
-        this.myTimesheet = page.getByRole('button', {name: 'My Timesheet'});
+        this.latestPosts = page.getByText('Buzz Latest Posts'); // Locates widget "Buzz Latest Posts"
+
+        this.quickLaunch = page.getByText('Quick Launch'); // Locates widget "Quick Launch"
+
+        this.buzzLatestPosts = page.getByText('Buzz Latest Posts'); // Locates widget "Buzz Latest Posts"
+
+        this.employeesOnLeave = page.getByText('Employees on Leave Today'); // Locates widget "Employees on Leave Today"
+
+        this.employeeDistributionSubUnit = page.getByText('Employee Distribution by Sub Unit'); // Locates widget "Employee Distribution by Sub Unit"
+
+        this.employeeDistributionLocation = page.getByText('Employee Distribution By Location'); // Locates widget "Employee Distribution by Location"
     };
 
 
