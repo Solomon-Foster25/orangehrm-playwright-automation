429019c0ce1015c2b67b2bee99df48b5907da5d6
Solomon Foster <solomon.foster25@gmail.com>
2026-06-17 10:30:06 -0600

    Fixed locator spacing

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -4,8 +4,11 @@ export class DashboardPage extends BasePage {
     // Initialize locators in constructor
 
     constructor(page) {
+
         super(page); // Initializes page
+        
         this.dashboardTitle = page.getByRole('heading', {name: 'Dashboard'}); // Locates title 'Dashboard'
+        
         this.timeAtWork = page.getByText('Time at Work'); // Locates widget 'Time at Work'
                 
         this.myActions = page.getByText('My Actions'); // Locates widget "My Actions"
@@ -14,13 +17,11 @@ export class DashboardPage extends BasePage {
 
         this.quickLaunch = page.getByText('Quick Launch'); // Locates widget "Quick Launch"
 
-        this.buzzLatestPosts = page.getByText('Buzz Latest Posts'); // Locates widget "Buzz Latest Posts"
-
         this.employeesOnLeave = page.getByText('Employees on Leave Today'); // Locates widget "Employees on Leave Today"
 
         this.employeeDistributionSubUnit = page.getByText('Employee Distribution by Sub Unit'); // Locates widget "Employee Distribution by Sub Unit"
 
-        this.employeeDistributionLocation = page.getByText('Employee Distribution By Location'); // Locates widget "Employee Distribution by Location"
+        this.employeeDistributionLocation = page.getByText('Employee Distribution by Location'); // Locates widget "Employee Distribution by Location"
     };
 
 
