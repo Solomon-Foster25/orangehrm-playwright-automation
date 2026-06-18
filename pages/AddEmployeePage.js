d60273572701a4bbfe4a3bf7dad131ebf1595a06
Solomon Foster <solomon.foster25@gmail.com>
2026-06-18 15:20:52 -0600

    Created POM for PIM add employee page

---
+++ pages/AddEmployeePage.js
@@ -0,0 +1,30 @@
+import { BasePage } from "./BasePage";
+
+export class AddEmployeePage extends BasePage {
+
+    constructor(page) {
+        super(page);
+
+        this.firstName = page.getByRole('textbox', {name: 'First Name'});
+        this.middleName = page.getByRole('textbox', {name: 'Middle Name'});
+        this.lastName = page.getByRole('textbox', {name: 'Last Name'});
+        this.employeeId = page.getByRole('textbox').nth(4);
+        this.saveButton = page.getByRole('button', { name: 'Save' });
+        this.requiredMessage = page.getByText('Required', { exact: true });
+    }
+
+    async goto() {
+        await this.navigate('/web/index.php/pim/addEmployee');
+    };
+
+    async addEmployee(first, middle, last, id) {
+        await this.firstName.fill(first);
+        if (middle) await this.middleName.fill(middle);
+        await this.lastName.fill(last);
+        if (id) {
+            await this.employeeId.fill(id);  // overrides the auto-populated ID
+        }
+        await this.saveButton.click();
+    };
+
+} 
