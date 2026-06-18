dae884d3829a42d5cdd1a95c9a290a2440891bfc
Solomon Foster <solomon.foster25@gmail.com>
2026-06-18 12:46:50 -0600

    Created POM for PIM employee list page

---
+++ pages/EmployeeListPage.js
@@ -0,0 +1,35 @@
+import { BasePage } from "./BasePage"
+
+export class EmployeeListPage extends BasePage {
+
+    constructor(page) {
+        super(page);
+        
+        this.searchByName = page.getByRole('textbox', { name: 'Type for hints...' });
+        this.employeeID = page.getByRole('textbox').nth(2);
+        this.searchButton = page.getByRole('button', { name: 'Search' });
+        this.resetButton = page.getByRole('button', { name: 'Reset' });
+        this.resultsTable = page.getByRole('table');
+        this.resultsRow = page.getByRole('row');
+        this.noRecordsFound = page.getByText('No Records Found');
+        this.addButton = page.getByRole('button', { name: 'Add'});
+    }
+
+    async goto() {
+        await this.navigate('/web/index.php/pim/viewEmployeeList')
+    };
+
+    async searchByName(name) {
+        await this.searchByName.fill(name);
+        await this.searchButton.click();
+    };
+
+    async searchById(id) {
+        await this.employeeID.fill(id);
+        await this.searchButton.click();
+    };
+
+    resultRowContaining(text) {
+        return this.resultsRow.filter({ hasText: text });
+    };       
+}
