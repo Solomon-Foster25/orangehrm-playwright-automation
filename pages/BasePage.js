0645e4734f404941e901ac6445c14199469948e0
Solomon Foster <solomon.foster25@gmail.com>
2026-06-19 19:43:17 -0600

    Add waitForSpinner method, for loading waits.

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -8,4 +8,11 @@ export class BasePage {
     async navigate(path) {
         await this.page.goto(path);
     };
+
+    async waitForSpinner() {
+        const spinner = this.page.locator('.oxd-loading-spinner');
+        // wait for it to show (briefly), then wait for it to go away
+        await spinner.waitFor({ state: 'visible', timeout: 2000 }).catch(() => {});
+        await spinner.waitFor({ state: 'hidden' }).catch(() => {});
+    }
 }
